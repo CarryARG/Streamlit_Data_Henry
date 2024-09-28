b40f89ebf3dc23bfe19246e6781c5b87d0fea856
CarryARG <lucaas_carranza@hotmail.com>
2024-09-28 20:18:17 -0300

    prueba

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -189,21 +189,13 @@ st.markdown("""
 
 
 # HTML para el Navbar utilizando Bootstrap con los botones personalizados
-
 st.markdown(f"""
-
     <nav class="navbar-custom">
-
-        <a href="#" class="nav-item" onclick="window.location.href='/?page=home'">Home</a>
-
-        <a href="#" class="nav-item" onclick="window.location.href='/?page=dashboard'">Dashboard</a>
-
-        <a href="#" class="nav-item" onclick="window.location.href='/?page=modelos'">Modelos</a>
-
+        <a href="#" class="nav-item" onclick="window.location.href='/?page=home'; window.location.reload()">Home</a>
+        <a href="#" class="nav-item" onclick="window.location.href='/?page=dashboard'; window.location.reload()">Dashboard</a>
+        <a href="#" class="nav-item" onclick="window.location.href='/?page=modelos'; window.location.reload()">Modelos</a>
     </nav>
-
 """, unsafe_allow_html=True)
-
 
 
 
