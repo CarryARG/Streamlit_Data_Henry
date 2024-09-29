3fc1206d07ae1a22deae7d42fd0c1f6a153e7315
CarryARG <lucaas_carranza@hotmail.com>
2024-09-28 22:18:44 -0300

    prueba

--- Streamlit/home.py
+++ Streamlit/home.py
@@ -23,7 +23,7 @@ def home_page():
         }
         h1 { color: #F25A38; }
         h2 { color: #F25041; }
-        h3 { color: #FFF; }
+        h3 { color: #000000; }
         .card {
             background-color: #56B5BF;
             border-radius: 10px;
