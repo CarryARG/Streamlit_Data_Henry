74111b5849f71d9108f7bca7afdf46558397aac6
CarryARG <lucaas_carranza@hotmail.com>
2024-09-28 21:03:17 -0300

    prueba

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -156,17 +156,17 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 
-
-
-
 # Navbar HTML
 st.markdown(f"""
     <nav class="navbar-custom">
-        <a href="javascript:window.location.search = '?page=home'" class="nav-item">Home</a>
-        <a href="javascript:window.location.search = '?page=dashboard'" class="nav-item">Dashboard</a>
-        <a href="javascript:window.location.search = '?page=modelos'" class="nav-item">Modelos</a>
+        <a href="javascript:window.location.search ='?page=home'" class="nav-item">Home</a>
+        <a href="javascript:window.location.search ='?page=dashboard'" class="nav-item">Dashboard</a>
+        <a href="javascript:window.location.search ='?page=modelos'" class="nav-item">Modelos</a>
     </nav>
 """, unsafe_allow_html=True)
+
+
+
 
 
 # Cargar el contenido de la página basada en el valor del parámetro 'page'
