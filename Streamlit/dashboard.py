66008379ba07a2f49e1d2e4edd672a4fc82cf7d2
CarryARG <lucaas_carranza@hotmail.com>
2024-09-29 00:32:51 -0300

    prueba

--- Streamlit/dashboard.py
+++ Streamlit/dashboard.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import home  # Importar la página 'home.py'
+import dashboard
+import modelos
 
 # Definir función principal para manejar pestañas
 def dashboard_page():
