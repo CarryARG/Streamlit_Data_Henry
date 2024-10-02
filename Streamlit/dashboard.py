cbe385c04cab4147e3725fc3199661c06093fcc1
Lucas Daniel Carranza <110673534+CarryARG@users.noreply.github.com>
2024-10-02 19:46:33 -0300

    Update dashboard.py

--- Streamlit/dashboard.py
+++ Streamlit/dashboard.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import home  # Importar la página 'home.py'
-import dashboard
-import modelos
+import streamlit as st
+import acercaDe  # Importar la página 'home.py'
+import dashboard  # Importar la página 'dashboard.py'
+import modelos  # Importar la página 'modelos_ml.py'
+import inicio # Importar la página 'inicio.py'
+import base64
 
 # Definir función principal para manejar pestañas
 def dashboard_page():
