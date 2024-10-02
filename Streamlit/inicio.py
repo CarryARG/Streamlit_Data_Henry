8bc1a620e69bf4dab1677e781a6267cd7654a50b
Lucas Daniel Carranza <110673534+CarryARG@users.noreply.github.com>
2024-10-02 19:46:52 -0300

    Update inicio.py

--- Streamlit/inicio.py
+++ Streamlit/inicio.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import home  # Importar la página 'home.py'
-import dashboard
-import modelos
+import streamlit as st
+import acercaDe  # Importar la página 'home.py'
+import dashboard  # Importar la página 'dashboard.py'
+import modelos  # Importar la página 'modelos_ml.py'
+import inicio # Importar la página 'inicio.py'
 import base64
 from PIL import Image
 
