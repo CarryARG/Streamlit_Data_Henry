14da4a0f057ab6764f0ab14bb035605ca02c9463
Lucas Daniel Carranza <110673534+CarryARG@users.noreply.github.com>
2024-10-02 19:47:15 -0300

    Update modelos.py

--- Streamlit/modelos.py
+++ Streamlit/modelos.py
@@ -1,7 +1,8 @@
 import streamlit as st
-import home  # Importar la página 'home.py'
-import dashboard
-import modelos
+import acercaDe  # Importar la página 'home.py'
+import dashboard  # Importar la página 'dashboard.py'
+import modelos  # Importar la página 'modelos_ml.py'
+import inicio # Importar la página 'inicio.py'
 import base64
 from PIL import Image
 
