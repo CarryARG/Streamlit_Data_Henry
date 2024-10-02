e974faef4055d50336ea1d906ea6d3f387f72c67
Lucas Daniel Carranza <110673534+CarryARG@users.noreply.github.com>
2024-10-02 19:46:01 -0300

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -1,13 +1,8 @@
 import streamlit as st
-
 import acercaDe  # Importar la página 'home.py'
-
 import dashboard  # Importar la página 'dashboard.py'
-
 import modelos  # Importar la página 'modelos_ml.py'
-
 import inicio # Importar la página 'inicio.py'
-
 import base64
 
 
