f1740aad19b2eef3b2e1804dea0e83a77d449696
Lucas Daniel Carranza <110673534+CarryARG@users.noreply.github.com>
2024-10-03 19:42:46 -0300

    Update inicio.py

--- Streamlit/inicio.py
+++ Streamlit/inicio.py
@@ -15,10 +15,10 @@ def get_image_b64(image_path):
         st.error(f"Image not found at {image_path}")
         return None
 
-<div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh; background: url('background-image-url.jpg') no-repeat center center fixed; background-size: cover;">
+<div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh; background: url('./Streamlit/images/wallpaper_uber.png') no-repeat center center fixed; background-size: cover;">
   <div class="card text-center" style="width: 18rem; background-color: rgba(0, 0, 0, 0.7); border: none;">
     <div class="card-body">
-      <img src="logo.png" class="rounded-circle" style="width: 150px; height: 150px; object-fit: cover;" alt="Logo">
+      <img src="./Streamlit/images/uber_logo.png" class="rounded-circle" style="width: 150px; height: 150px; object-fit: cover;" alt="Logo">
       <h5 class="card-title text-white mt-3">Nick Perez</h5>
       <p class="card-text text-white">Ingeniero de Software - Experto UI/UX</p>
       <div>
