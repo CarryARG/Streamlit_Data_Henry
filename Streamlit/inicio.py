64992058ccff450ad40431375c1b04b0f7152dba
CarryARG <lucaas_carranza@hotmail.com>
2024-10-02 22:04:12 -0300

    prueba

--- Streamlit/inicio.py
+++ Streamlit/inicio.py
@@ -1,18 +1,15 @@
 import streamlit as st
-import acercaDe  # Importar la página 'home.py'
-import dashboard  # Importar la página 'dashboard.py'
-import modelos  # Importar la página 'modelos_ml.py'
-import inicio # Importar la página 'inicio.py'
-import base64
+import acercaDe  # Import the page 'home.py'
+import dashboard  # Import the page 'dashboard.py'
+import modelos  # Import the page 'modelos_ml.py'
+import inicio  # Import the page 'inicio.py'
 from PIL import Image
 
-
 def set_background(png_file):
-    # Cargar la imagen y codificarla en base64
+    """Loads the image and sets it as the background using CSS."""
     with open(png_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read()).decode()
 
-    # Insertar la imagen en el fondo usando CSS y base64
     st.markdown(
         f"""
         <style>
@@ -25,27 +22,31 @@ def set_background(png_file):
         }}
         </style>
         """,
-        unsafe_allow_html=True
+        unsafe_allow_html=True,
     )
 
 
-
-
-
-
 def inicio_page():
-    # Establecer fondo usando la imagen local codificada en base64
+    """Displays the 'inicio' page with the logo from the specified path."""
+
+    # Set the background using the set_background function
     set_background('./Streamlit/images/wallpaper_uber.png')
 
-    # Estructura de la página con Bootstrap y centrado
-    st.markdown("""
+    # Structure the page with Bootstrap and center the content
+    st.markdown(
+        """
         <div class="container-fluid d-flex justify-content-center align-items-center" style="height: 100vh;">
             <div class="row text-center">
                 <div class="col">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="Uber Logo" style="width: 150px;">
-                    <h1 class="mt-3" style="color: #56B5BF;">Bienvenido a nuestro proyecto Uber</h1>
+                    <img src="./Streamlit/images/uber_logo.png" alt="Uber Logo" style="width: 150px;">  <h1 class="mt-3" style="color: #56B5BF;">Bienvenido a nuestro proyecto Uber</h1>
                     <p style="color: #F2F2F2;">Explorando la revolución del transporte con análisis y predicciones para mejorar la experiencia del usuario.</p>
                 </div>
             </div>
         </div>
-    """, unsafe_allow_html=True)
+        """,
+        unsafe_allow_html=True,
+    )
+
+
+# Assuming your app starts from the 'inicio' page
+inicio_page()
